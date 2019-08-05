50588572fae33bafeaa85682b38a4142bb039ef5
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-08-05 10:30:35 +0900

    app_models_user.rb fix indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,22 @@ class User < ApplicationRecord
 
   def self.import(path)
     list = []
-    CSV.foreach(path,headers: true) do |row|
-      list << {
-          name: row["name"],　#カンマ忘れずに
-          age: row["age"],  #スペルミス気をつけて
-          address: row["address"]
-        }
-    end
+      CSV.foreach(path,headers: true) do |row|
+        list << {
+            name: row["name"], #カンマ忘れずに
+            age: row["age"],  #スペルミス気をつけて
+            address: row["address"]
+          }
+      end
 
     #begin
 
-     puts "インポート開始"
-     User.create!(list)
-     puts "インポートに成功しました"
-    rescue ActiveModel::unknownAttributeError => invalid
-
-     puts "インポートに失敗しました:#{invalid}"
+      puts "インポート開始"
+        User.create!(list)
+      puts "インポートに成功しました"
+        rescue ActiveModel::unknownAttributeError => invalid
+      puts "インポートに失敗しました:#{invalid}"
 
     #end
-
   end
-
 end
